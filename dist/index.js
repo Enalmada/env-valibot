e4e8c48a4e242b40b612dfa9eef8a7b428480533
Adam Lane <enalmada@gmail.com>
2023-09-25 11:17:53 -0700

    bun build

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,29 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateEnv = exports.required = exports.createEnvSchema = void 0;
-var EnvValibot_1 = require("./EnvValibot");
-Object.defineProperty(exports, "createEnvSchema", { enumerable: true, get: function () { return EnvValibot_1.createEnvSchema; } });
-Object.defineProperty(exports, "required", { enumerable: true, get: function () { return EnvValibot_1.required; } });
-Object.defineProperty(exports, "validateEnv", { enumerable: true, get: function () { return EnvValibot_1.validateEnv; } });
+// src/EnvValibot.ts
+import {minLength, object, safeParse, string} from "valibot";
+function validateEnv(schema, envVars, skipEnvValidation = "false") {
+  if (skipEnvValidation !== "true") {
+    const parsed = safeParse(schema, envVars);
+    if (!parsed.success) {
+      const reducedIssues = reduceIssues(parsed.issues);
+      console.error("Issue with environment variables: " + JSON.stringify(reducedIssues));
+      process.exit(1);
+    }
+    return parsed;
+  } else {
+    return;
+  }
+}
+var reduceIssues = function(issues) {
+  return issues.map((issue) => ({
+    attribute: issue.path?.[0].key,
+    input: issue.input,
+    message: issue.message
+  }));
+};
+var required = (key) => string([minLength(1, `${key} required`)]);
+var createEnvSchema = (schemaDefinition) => object(schemaDefinition);
+export {
+  validateEnv,
+  required,
+  createEnvSchema
+};
