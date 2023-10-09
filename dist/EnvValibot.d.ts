5da7be5911788de1c6c9ac42e4e74b17b72d7204
Adam Lane <enalmada@gmail.com>
2023-10-09 12:29:32 -0700

    build files

--- dist/EnvValibot.d.ts
+++ dist/EnvValibot.d.ts
@@ -1,10 +1,12 @@
-import { Issue, SafeParseResult } from 'valibot';
-export declare const required: (key: string) => any;
-export declare const createEnvSchema: (schemaDefinition: Record<string, any>) => any;
+import { object, string, type Issue, type SafeParseResult as ValibotSafeParseResult } from 'valibot';
+type Schema = ReturnType<typeof object> | ReturnType<typeof string>;
+export declare const required: (key: string) => ReturnType<typeof string>;
+export declare const createEnvSchema: (schemaDefinition: Record<string, Schema>) => ReturnType<typeof object>;
 export interface ReducedIssue {
     attribute: string | undefined;
     input: unknown;
     message: string;
 }
-export declare function validateEnv(schema: any, envVars: Record<string, unknown>, skipEnvValidation?: string | undefined): SafeParseResult<any> | undefined;
+export declare function validateEnv(schema: Schema, envVars: Record<string, unknown>, skipEnvValidation?: string | undefined): ValibotSafeParseResult<Schema> | undefined;
 export declare function reduceIssues(issues: Issue[]): ReducedIssue[];
+export {};
