7c1fdbb9725b12de6e5289a52b4cb6f0036d5f28
Adam Lane <enalmada@gmail.com>
2023-12-05 16:15:41 -0800

    valibot v1 schemas

--- src/EnvValibot.ts
+++ src/EnvValibot.ts
@@ -5,18 +5,57 @@ import {
   safeParse,
   string,
   type BaseSchema,
+  type EnumSchema,
+  type InstanceSchema,
+  type IntersectSchema,
   type Issue,
+  type NanSchema,
+  type NeverSchema,
+  type NonNullableSchema,
+  type NonNullishSchema,
+  type NonOptionalSchema,
+  type NullableSchema,
+  type NullishSchema,
+  type NumberSchema,
+  type OptionalSchema,
+  type PicklistSchema,
+  type RecursiveSchema,
+  type SpecialSchema,
+  type StringSchema,
+  type UnionSchema,
+  type UnknownSchema,
   type SafeParseResult as ValibotSafeParseResult,
+  type VoidSchema,
 } from 'valibot';
 
-type Schema = BaseSchema<any, any>;
-
-export const required = (key: string): ReturnType<typeof string> =>
-  string([minLength(1, `${key} required`)]);
+type Schema =
+  | BaseSchema<any, any>
+  | StringSchema<any>
+  | NumberSchema<any>
+  | PicklistSchema<any>
+  | OptionalSchema<any, any>
+  | EnumSchema<any>
+  | InstanceSchema<any>
+  | IntersectSchema<any>
+  | NanSchema
+  | NeverSchema
+  | NonNullableSchema<any>
+  | NonNullishSchema<any>
+  | NonOptionalSchema<any>
+  | NullableSchema<any>
+  | NullishSchema<any>
+  | RecursiveSchema<any>
+  | SpecialSchema<any>
+  | UnionSchema<any>
+  | UnknownSchema
+  | VoidSchema;
 
 export const createEnvSchema = (
   schemaDefinition: Record<string, Schema>
 ): ReturnType<typeof object> => object(schemaDefinition as any);
+
+export const required = (key: string): ReturnType<typeof string> =>
+  string([minLength(1, `${key} required`)]);
 
 export interface ReducedIssue {
   attribute: string | undefined;
